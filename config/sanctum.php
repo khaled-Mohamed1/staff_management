0d72435c6ba437454297411da35f8c0984628003
KhaledMohammed <khaled.1m2h3o@gmail.com>
2023-02-19 22:19:30 +0200

    token 60s

--- config/sanctum.php
+++ config/sanctum.php
@@ -48,6 +48,9 @@ return [
 
     'expiration' => null,
 
+    'personal_access_tokens' => [
+        'lifetime' => 60, // lifetime set to 1 hour
+    ],
     /*
     |--------------------------------------------------------------------------
     | Sanctum Middleware
