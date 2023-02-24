ea12a171ee8ed94988be5cc4e8c5efea185643a6
KhaledMohammed <khaled.1m2h3o@gmail.com>
2023-02-24 18:30:45 +0200

    test webhook 6

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -541,14 +541,7 @@ class MessageController extends Controller
     public function verifyWebhook(Request $request){
         try {
 
-            $data = file_get_contents("php://input");
-            $event = json_decode($data, true);
-            if(isset($event)){
-                //Here, you now have event and can process them how you like e.g Add to the database or generate a response
-                $file = 'log.txt';
-                $data =json_encode($event)."\n";
-                file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-            }
+
 
             $verifyToken = 'wibblewebhook4!';
             $query = $request->query();
