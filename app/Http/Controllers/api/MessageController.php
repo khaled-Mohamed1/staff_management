e82a9c7ec2b29186f736882d4ad391b61e2794e9
KhaledMohammed <khaled.1m2h3o@gmail.com>
2023-02-24 18:51:07 +0200

    test webhook 12

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -579,8 +579,14 @@ class MessageController extends Controller
             if(!empty($value['messages'])){
                 if($value['messages'][0]['type'] == 'text'){
                     $body = $value['messages'][0]['text']['body'];
-                    $file = 'log.txt';
-                    file_put_contents($file, $body, FILE_APPEND | LOCK_EX);
+                    $data = file_get_contents($request->getContent());
+                    $event = json_decode($data, true);
+                    if(isset($event)){
+                        //Here, you now have event and can process them how you like e.g Add to the database or generate a response
+                        $file = 'log.txt';
+                        $data =json_encode($event)."\n";
+                        file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+                    }
                 }
             }
 
